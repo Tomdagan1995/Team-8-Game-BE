425b26222dd63d6a89b2a8255eb8f79fa07e31b1
sarahitsko <112071488+sarahitsko@users.noreply.github.com>
2023-01-31 13:45:50 +0200

    add_score_api

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 const PORT = process.env.PORT || 3000;
-const signup = require('./routes/signup')
-const cors = require('cors');
+const signup = require("./routes/signup");
+const scoreRoutes = require("./routes/scoreRoutes");
+const cors = require("cors");
 const app = express();
 
 console.log(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/signup", signup);
 // app.use("/login", login);
+app.use("/scores", scoreRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
