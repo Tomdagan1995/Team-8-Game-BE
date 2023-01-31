b3aeb997e567a835e4d5c9eb29eb39811d563752
sarahitsko <112071488+sarahitsko@users.noreply.github.com>
2023-01-31 22:37:56 +0200

    All api scores routes

--- routes/scoreRoutes.js
+++ routes/scoreRoutes.js
@@ -4,5 +4,26 @@ const ScoresController = require("../controllers/ScoresController");
 const router = express.Router();
 
 router.post("/", ScoresController.AddScore);
+// Get All Users Score History API
+// Route: ‘/scores [GET] (Protected to user only)
+// Responsible for getting all the users scores
+router.get("/", ScoresController.getAllScores);
+// Get Logged In User’s Score history API
+// Route: ‘/scores/:id [GET]
 
+// ****** (Protected to user only) *********************** Add auth from iliya after push pull
+
+// Responsible for getting the logged in user's score
+router.get("/:id", ScoresController.getUserScores);
+// Route: ‘/scores/last/:id [GET] (Protected to user only)
+// Responsible for getting the user’s last score
+// Display the users last score in the home or game page
+router.get("/last/:id", ScoresController.getLastScore);
+// Route: ‘/scores/high/:id [GET]
+
+//********** */ (Protected to user only) ****************** Add auth from iliya after push pull
+
+// Responsible for getting highest score of a user
+// Display the user’s highest score in the home or game page
+router.get("/high/:id", ScoresController.getHighScore);
 module.exports = router;
