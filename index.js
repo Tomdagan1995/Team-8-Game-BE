79f2514dcc0e3ba650ad8c3ee51a0131074c1d37
zimin-iliya <zimin.box@gmail.com>
2023-01-31 14:51:17 +0200

    score and log in works, but still need to fix the

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const signup = require("./routes/signup");
 const scoreRoutes = require("./routes/scoreRoutes");
+const login = require("./routes/login");
 const cors = require("cors");
 const app = express();
 
@@ -11,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/signup", signup);
-// app.use("/login", login);
+app.use("/login", login);
 app.use("/scores", scoreRoutes);
 
 app.listen(PORT, () => {
